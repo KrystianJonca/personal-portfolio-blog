19a3338373ccccc11e80b5f415adc39cee2e4df5
Wionek <krystianjonca43@gmail.com>
2022-05-16 16:24:39 +0200

    Removed unused schemas

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -6,9 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
 import blockContent from './blockContent';
-import category from './category';
 import post from './post';
-import author from './author';
 
 import skill from './skill';
 import project from './project';
@@ -23,8 +21,6 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     post,
-    author,
-    category,
     skill,
     project,
     // When added to this list, object types can be used as
