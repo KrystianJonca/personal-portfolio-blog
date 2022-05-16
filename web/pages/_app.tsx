c282548d345fab938c1f2d9eedfb9c59fd6ac681
Wionek <krystianjonca43@gmail.com>
2022-05-16 20:11:22 +0200

    Added page transitions

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,13 +1,45 @@
 import type { AppProps } from 'next/app';
 import { Layout } from '../components';
 import NextNProgress from 'nextjs-progressbar';
+import { AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps }: AppProps) {
+if (typeof window !== 'undefined') {
+  window.history.scrollRestoration = 'manual';
+}
+
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 20 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: -0, y: 20 },
+};
+
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <NextNProgress color="#ecf0f1" />
       <Layout>
-        <Component {...pageProps} />
+        <motion.main
+          key={router.route}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 0.4, type: 'easeInOut' }}
+          style={{ position: 'relative' }}
+        >
+          <AnimatePresence
+            exitBeforeEnter
+            initial={true}
+            onExitComplete={() => {
+              if (typeof window !== 'undefined') {
+                window.scrollTo({ top: 0 });
+              }
+            }}
+          >
+            <Component {...pageProps} />
+          </AnimatePresence>
+        </motion.main>
       </Layout>
     </>
   );
