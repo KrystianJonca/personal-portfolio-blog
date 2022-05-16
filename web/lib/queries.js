9dd18e106e80698fcacc1865a633bf6b8616a1b9
Wionek <krystianjonca43@gmail.com>
2022-05-16 21:21:01 +0200

    Added queries for sanity

--- web/lib/queries.js
+++ web/lib/queries.js
@@ -1,8 +1,8 @@
 import { groq } from 'next-sanity';
 
-export const querySkills = groq`[_type == "skill"]`;
-export const queryLatestPosts = groq`[_type == "post"] | order(_createdAt desc)[0...4] { title, slug, mainImage }`;
-export const queryPosts = groq`[_type == "post"] | order(_createdAt desc) { title, slug, mainImage }`;
-export const queryPost = groq`[_type == "post" && slug.current == $pageSlug][0]`;
-export const queryHighlightedProjects = groq`[_type == "project"][0...4]`;
-export const queryProjects = groq`[_type == "project"]`;
+export const querySkills = groq`*[_type == "skill"] | order(_createdAt asc) { tag }`;
+export const queryLatestPosts = groq`*[_type == "post"] | order(_createdAt desc)[0...4] { title, slug, mainImage }`;
+export const queryPosts = groq`*[_type == "post"] | order(_createdAt desc) { title, slug, mainImage }`;
+export const queryPost = groq`*[_type == "post" && slug.current == $pageSlug][0] { title,slug, mainImage, body, _createdAt }`;
+export const queryHighlightedProjects = groq`*[_type == "project"][0...4] { name, image, Link, techstack, desc }`;
+export const queryProjects = groq`*[_type == "project"] { name, image, Link, techstack, desc }`;
