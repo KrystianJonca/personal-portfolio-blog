509ceeabd4975475254403e250855eecce1c7ec1
Wionek <krystianjonca43@gmail.com>
2022-05-16 19:25:01 +0200

    Added posts page

---
+++ web/pages/blog/index.tsx
@@ -0,0 +1,60 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { getClient } from '../../lib/sanity.server';
+import { queryPosts } from '../../lib/queries';
+import { GetStaticProps } from 'next';
+import { Posts } from '../../components';
+import { PreviewPost } from '../../typings';
+
+interface IHomeProps {
+  posts: Array<PreviewPost>;
+}
+
+const Blog: NextPage = ({ posts }) => {
+  return (
+    <>
+      <Head>
+        <title>Blog Posts | Krystian Jonca</title>
+      </Head>
+
+      <header>
+        <h2 className="page-title">Posts</h2>
+      </header>
+
+      <main>
+        <Posts posts={posts} />
+      </main>
+
+      <style jsx global>
+        {`
+          .page-title {
+            font-weight: 400;
+            font-size: 4rem;
+            margin: 2rem;
+            text-align: center;
+          }
+          main {
+            max-width: 540px;
+            margin: 0 auto;
+            margin-bottom: 2rem;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const sanityClient = await getClient();
+
+  const posts = await sanityClient.fetch(queryPosts);
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 60,
+  };
+};
+
+export default Blog;
